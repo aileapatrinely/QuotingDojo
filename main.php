3963acc4d4e2099a64363f071579a619fb62492b
Ailea Patrinely <aileapatrinely@Aileas-MacBook-Air.local>
2021-07-21 13:45:14 -0500

    creates if astatement and foreach loop to print out quotes, name, and date

--- main.php
+++ main.php
@@ -9,10 +9,14 @@
 <body>
     <h1>This is where I keep m'quotes.</h1>
     <!-- some php with if statment and for loop and then the quotes -->
-    <?php 
-    //need an if isset
-    foreach($_SESSION['quotes'] as $quote)
-    //print out them quotes, name, and date info
-    ?>
+    <?php if(isset($_SESSION['quotes'])){
+        foreach($_SESSION['quotes'] as $quote){ ?>
+        <!-- print out them quotes, name, and date info -->
+        <div>
+            <h3>"<?= $quote['quote']?>"</h3>
+            <h5>- <?= $quote['name'] ?> at <?= date_format(date_create($quote['created_at']), 'h:i a M d Y')?></h5>
+        </div>
+        }
+    } ?>
 </body>
 </html>
